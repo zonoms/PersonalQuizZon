6885c458a7641918d3f9078695c023028fd050c7
zon <zon_click@mail.ru>
2020-06-17 23:18:27 +0300

    1:22:34

---
+++ PersonalQuizZon/ViewControlles/ResultsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ResultsViewController.swift
+//  PersonalQuizZon
+//
+//  Created by ADMIN on 17.06.2020.
+//  Copyright © 2020 Arkadij Ryabov. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
+
